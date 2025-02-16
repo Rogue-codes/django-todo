0a6e2e1897a491f545dd68e3398e3e2cbdde945a
nnamdi <nnamdidanielosuji@gmail.com>
2025-02-16 06:03:41 +0100

    feat: returned user object at the point of login

--- todoapi/serializers.py
+++ todoapi/serializers.py
@@ -1,37 +1,39 @@
 from rest_framework import serializers
 from .models import CustomUser, Task
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'first_name', 'last_name',
-                  'phone_number', 'address', 'is_active', 'created_at']
+                  'phone_number', 'address', 'is_active', 'created_at', 'password']
         extra_kwargs = {
             'password': {'write_only': True},
         }
 
-        def create(self, validated_data):
-            email = validated_data['email']
-            first_name = validated_data['first_name']
-            username = validated_data['username']
-            last_name = validated_data['last_name']
-            phone_number = validated_data['phone_number']
-            address = validated_data['address']
-            password = validated_data['password']
+    def create(self, validated_data):
+        email = validated_data['email']
+        first_name = validated_data['first_name']
+        username = validated_data['username']
+        last_name = validated_data['last_name']
+        phone_number = validated_data['phone_number']
+        address = validated_data['address']
+        password = validated_data['password']
 
-            user = get_user_model()
-            new_user = user(email=email, username=username, phone_number=phone_number,
-                            last_name=last_name, first_name=first_name, address=address)
-            new_user.set_password(password)
-            new_user.save()
+        user = get_user_model()
+        new_user = user(email=email, username=username, phone_number=phone_number,
+                        last_name=last_name, first_name=first_name, address=address)
+        new_user.set_password(password)
+        new_user.save()
+        return new_user
 
 
 class CreateTaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['user', 'title', 'description', 'status',
+        fields = ['title', 'description', 'status',
                   'start_date', 'start_time', 'end_date', 'end_time']
 
     def validate(self, data):
@@ -42,7 +44,7 @@ class CreateTaskSerializer(serializers.ModelSerializer):
 
         if end_date < start_date:
             raise serializers.ValidationError(
-                {"end_date": "End date cannot be before start date."}) 
+                {"end_date": "End date cannot be before start date."})
 
         # Ensure end_time is not before start_time if the task is on the same day
         if start_date == end_date and end_time < start_time:
@@ -52,10 +54,32 @@ class CreateTaskSerializer(serializers.ModelSerializer):
         return data
 
     def create(self, validated_data):
+        request = self.context.get('request')  # Get request context
+        validated_data['user'] = request.user
         return Task.objects.create(**validated_data)
-    
+
+
 class GetTaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['user', 'task_id', 'title', 'description', 'status',
                   'start_date', 'start_time', 'end_date', 'end_time']
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # Include user data
+        data["user"] = {
+            "id": self.user.id,
+            "username": self.user.username,
+            "email": self.user.email,
+            "first_name": self.user.first_name,
+            "last_name": self.user.last_name,
+            "phone_number": self.user.phone_number,
+            "address": self.user.address,
+            "is_active": self.user.is_active,
+        }
+
+        return data
