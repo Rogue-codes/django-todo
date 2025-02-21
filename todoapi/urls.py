9062bc7b7a34ba60decda6ff4b99f3cb53b2a506
nnamdi <nnamdidanielosuji@gmail.com>
2025-02-21 04:17:49 +0100

    feat: update task status

--- todoapi/urls.py
+++ todoapi/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('task/create/', views.CreateTaskApiview.as_view()),
     path('tasks/all/', views.MyTaskListApiView.as_view(), name='tasks_list'),
     path('tasks/<uuid:task_id>', views.TaskDetailsView.as_view(), name='task'),
-]
+    path("tasks/<uuid:task_id>/status/",
+         views.UpdateTaskStatusView.as_view(), name="update_task_status"),
 
+]
